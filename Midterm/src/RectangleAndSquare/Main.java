fe809aa3c98e399e5ec6bd96360b7778e8badaef
Ly Nita <156105991+lyNita01@users.noreply.github.com>
2024-01-10 20:07:48 +0700

    Add files via upload

---
+++ Midterm/src/RectangleAndSquare/Main.java
@@ -0,0 +1,10 @@
+package RectangleAndSquare;
+
+    public class Main {
+        public static void main(String[] args) {
+            Square square = new Square(10.0);
+            System.out.println("Area of square: " + square.getArea());
+            System.out.println("Perimeter of square: " + square.getPerimeter());
+        }
+    }
+
---
+++ Midterm/src/RectangleAndSquare/Square.java
@@ -0,0 +1,7 @@
+package RectangleAndSquare;
+
+ class Square extends Rectangle {
+    public Square(double side) {
+        super(side, side);
+    }
+}
